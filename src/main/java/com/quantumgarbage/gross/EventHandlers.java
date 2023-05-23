ac1495299e7ced2b1fb7a80a66a7ad5c799ac467
fmbellomy <fmbellomy@quantumgarbage.com>
2023-05-23 06:10:55 -0500

    apparently you gotta do everything yourself around here

--- src/main/java/com/quantumgarbage/gross/EventHandlers.java
+++ src/main/java/com/quantumgarbage/gross/EventHandlers.java
@@ -1,16 +1,25 @@
 package com.quantumgarbage.gross;
 
-import static org.bukkit.entity.EntityType.ITEM_FRAME;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+
 import java.util.UUID;
+
+import static org.bukkit.entity.EntityType.ITEM_FRAME;
 public class EventHandlers implements Listener {
     private static final UUID CROW = UUID.fromString("490a39fc-f7f9-4aab-bb26-81e188bbd8aa");
     @EventHandler
-    public void badCrow(PlayerInteractAtEntityEvent e){
+    public void badCrow(PlayerInteractAtEntityEvent e) {
         boolean isCrow = (e.getPlayer().getUniqueId().equals(CROW));
         boolean isBad = (e.getRightClicked().getType().equals(ITEM_FRAME));
-        e.setCancelled(isCrow && isBad);
+        System.out.println("isCrow:" + isCrow);
+        System.out.println("isBad:" + isBad);
+        if (isCrow && isBad) {
+            e.setCancelled(true);
+            ItemFrame iframe = (ItemFrame) e.getRightClicked();
+            iframe.setRotation(iframe.getRotation().rotateCounterClockwise());
+        }
     }
 }
